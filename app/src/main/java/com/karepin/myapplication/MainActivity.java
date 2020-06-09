a5ac0daabe88766e0371207898537b8f9c09635a
Alex Karepin <akarepin@gmail.com>
2020-06-09 17:58:08 +0300

    Initial commit

---
+++ app/src/main/java/com/karepin/myapplication/MainActivity.java
@@ -0,0 +1,84 @@
+package com.karepin.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Random random = new Random();
+    private ItemsDataAdapter adapter;
+    private List<Drawable> images = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        ListView listView = findViewById(R.id.listView);
+
+        fillImages();
+
+        // При тапе по кнопке добавим один новый элемент списка
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomItemData();
+            }
+        });
+
+        // Создаем и устанавливаем адаптер на наш список
+        adapter = new ItemsDataAdapter(this, null);
+        listView.setAdapter(adapter);
+
+        // При тапе по элементу списка будем показывать его данные
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showItemData(position);
+            }
+        });
+    }
+    private void fillImages() {
+        images.add(getDrawable(R.drawable.ic_menu_report_image));
+        images.add(getDrawable(R.drawable.ic_menu_add));
+        images.add(getDrawable(R.drawable.ic_menu_agenda));
+        images.add(getDrawable(R.drawable.ic_menu_camera));
+        images.add(getDrawable(R.drawable.ic_menu_call));
+    }
+
+    // Создадим ну почти случайные данные для нашего списка.
+    // random.nextInt(граница_последнего_элемента)
+    // Для каждого элемента мы возьмем 1 случайную картинку
+    // из 5, которые мы сделали вначале.
+    private void generateRandomItemData() {
+        adapter.addItem(new ItemData(
+                images.get(random.nextInt(images.size())),
+                "Привет" + adapter.getCount(),
+                "выбери пункт",
+                random.nextBoolean()));
+    }
+
+    // Покажем сообщение с данными
+    private void showItemData(int position) {
+        ItemData itemData = adapter.getItem(position);
+        Toast.makeText(MainActivity.this,
+                "Title: " + itemData.getTitle() + "\n" +
+                        "Subtitle: " + itemData.getSubtitle() + "\n" +
+                        "Checked: " + itemData.isChecked(),
+                Toast.LENGTH_LONG).show();
+    }
+}
+
